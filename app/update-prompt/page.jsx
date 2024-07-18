dc54e89d32762089ae2a8af3927ae3cc76f1afc9
Jayash Yadav <jayash.yadav@geminisolutions.com>
2024-07-18 10:14:55 +0530

    suspense error walk around

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,64 +1,15 @@
-"use client";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+const UpdatePrompt = dynamic(() => import("@components/EditPrompt"), {
+  suspense: true,
+});
 
-import Form from "@components/Form";
-
-const UpdatePrompt = () => {
-  const router = useRouter();
-  const promptId = router.query.id;
-
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Missing PromptId!");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
+const Page = () => {
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense>
+      <EditPrompt />
+    </Suspense>
   );
 };
 
